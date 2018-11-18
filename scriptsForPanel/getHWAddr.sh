252bcc9f139178e869c2fb02b599463f6dd4022d
jakub <jakub1500@gmail.com>
2018-11-18 09:31:27 +0100

    Add new alias & fix for getHWAddr.sh

--- scriptsForPanel/getHWAddr.sh
+++ scriptsForPanel/getHWAddr.sh
@@ -5,6 +5,6 @@ if [[ -z $1 ]]; then
 	exit 1
 fi
 
-mac=$(ifconfig $1 | grep hwaddr | grep -Eo "([a-fA-F0-9]{2}:){5}[a-fA-F0-9]{2}")
+mac=$(ifconfig $1 | grep ether | grep -Eo "([a-fA-F0-9]{2}\:){5}[a-fA-F0-9]{2}")
 
 echo "$1	$mac"
